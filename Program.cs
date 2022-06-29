dc43442decf473771f4c5ff89497d139a6dfae01
AkraTimur <timur.akra@gmail.com>
2022-06-29 15:20:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пожалуйста, введите количества выбираемых карт");
+            string chooseNumbers = Console.ReadLine();
+            if(int.TryParse(chooseNumbers, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+            Console.ReadLine();
+        }
+    }
+}
